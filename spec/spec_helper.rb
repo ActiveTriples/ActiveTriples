3d2dbaf0ef2485f33d025a19368803988a65d35e
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-04-29 15:40:00 -0700

    configuring rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,14 @@
+require 'bundler/setup'
+Bundler.setup
+
 require 'active_triples'
 
 Dir['./spec/support/**/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :progress
+end
