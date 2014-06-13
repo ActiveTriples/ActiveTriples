ca883608c0009090f7a5d687e2f669cb2594acd1
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-06-13 03:44:28 -0700

    fixing deprecated color_enabled= in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'active_triples'
 Dir['./spec/support/**/*.rb'].each { |f| require f }
 
 RSpec.configure do |config|
-  config.color_enabled = true
+  config.color = true
   config.tty = true
 
   # Use the specified formatter
