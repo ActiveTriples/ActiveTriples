be95c1cef5df94edde5fdb872df029ad6dd4f5ac
MJ Suhonos <mj@suhonos.ca>
2014-09-12 15:04:29 -0400

    Replace accessors with ActiveModel hooks

--- lib/active_triples/identifiable.rb
+++ lib/active_triples/identifiable.rb
@@ -28,9 +28,29 @@ module ActiveTriples::Identifiable
     raise NotImplementedError
   end
 
+  ##
+  # Convenience method to return JSON-LD representation
+  def as_jsonld
+    resource.dump(:jsonld)
+  end
+
   private
     def resource_class
       self.class.resource_class
+    end
+
+    def write_attribute(attr_name, value)
+      resource.set_value(attr_name, value) if resource_class.properties.has_key? attr_name
+      super
+    end
+    
+    def set_relation(name, relation)
+      if resource_class.properties.has_key? name then
+        documents = relation.is_a?(Enumerable) ? relation : [relation]
+        documents.compact.each { |doc| resource.set_value(name, doc.resource.base_uri + doc.to_uri) }
+      end
+
+      super
     end
     
   public
@@ -40,13 +60,6 @@ module ActiveTriples::Identifiable
       delegate :configure, :properties, to: :resource_class
 
       def property(*args)
-        prop = args.first
-        define_method prop.to_s do 
-          resource.get_values(prop)
-        end
-        define_method "#{prop.to_s}=" do |*args|
-          resource.set_value(prop, *args)
-        end
         resource_class.property(*args)
       end
 
