85b7bbbaf8960e4bfa9186d92da47c5d66337639
Tom Johnson <tom@dp.la>
2015-02-15 14:52:00 -0800

    Adds parsing for existing Proxy List graphs

--- spec/active_triples/proxy_list_spec.rb
+++ spec/active_triples/proxy_list_spec.rb
@@ -1,21 +1,72 @@
 require 'spec_helper'
 
-class ActiveTriples::ProxyList
+##
+# Acts as a ORE style RDF ordered list.
+#
+# The implementation is backed by an `elements` array and builds an RDF::Graph
+# just-in-time.
+class RDF::ProxyList
   include Enumerable
   extend Forwardable
 
-  def_delegators :elements, :concat
+  def_delegators :elements, :empty?
 
-  def initialize(aggregator)
-    @aggregator = convert_to_aggregator(aggregator)
-    @elements = []
+  class << self
+    def first_from_graph(aggregator, graph)
+      matches = query_proxy_for(aggregator, graph, ORE['first'])
+      raise InvalidProxyListGraph if matches.length > 1
+      matches.first
+    end
+
+    def last_from_graph(aggregator, graph)
+      matches = query_proxy_for(aggregator, graph, ORE.last)
+      raise InvalidProxyListGraph if matches.length > 1
+      matches.first
+    end
+
+    def query_next_node(graph, current)
+      query = RDF::Query.new do
+        pattern [:current_proxy, ORE.proxyFor, current]
+        pattern [:current_proxy, ORE.next, :next_proxy]
+        pattern [:next_proxy, ORE.proxyFor, :next_node]
+      end
+
+      matches = query.execute(graph).map(&:next_node)
+      raise InvalidProxyListGraph if matches.length > 1
+      matches.first
+    end
+
+    private
+
+    ##
+    # Queries a given predicate with a proxyable value returning the node the
+    # proxy stands in for.
+    def query_proxy_for(aggregator, graph, predicate)
+      query = RDF::Query.new do
+        pattern [aggregator, predicate, :proxy]
+        pattern [:proxy, ORE.proxyFor, :value]
+      end
+
+      query.execute(graph).map(&:value)
+    end
   end
+
+  def initialize(aggregator, graph = nil)
+    @aggregator = convert_to_aggregator(aggregator)
+    @elements = elements_from_graph(graph)
+  end
+
   attr_reader :elements, :aggregator
   private :elements
 
   def <<(value)
     resource = convert_to_proxiable_element(value)
     elements << value
+  end
+
+  def concat(*args)
+    elements.concat(*args)
+    self
   end
 
   def each
@@ -67,6 +118,35 @@ class ActiveTriples::ProxyList
     end
   end
 
+  ##
+  # Creates an elements `Array` from a valid ORE Proxy ordered list.
+  #
+  # The implementation ignores all `ORE.prev` predicates, treating the graph as
+  # a singly linked list.
+  #
+  # @param graph [RDF::Queryable] A graph containing an ORE proxy list.
+  # @return [Array<RDF::Resource>] an Array of the resources in the proxy list
+  #
+  # @raise [InvalidProxyListGraph] if the graph's list is invalid
+  def elements_from_graph(graph)
+    return [] if graph.nil?
+
+    is_empty = self.class.first_from_graph(aggregator, graph).nil?
+    return is_empty ? [] : build_element_list(graph)
+  end
+
+  ##
+  # @see #elements_from_graph
+  def build_element_list(graph)
+    list = [self.class.first_from_graph(aggregator, graph)]
+
+    loop do
+      next_value = self.class.query_next_node(graph, list.last) || break
+      list << next_value
+    end
+    list
+  end
+
   public
 
   class UnproxiableObjectError < RuntimeError
@@ -74,53 +154,39 @@ class ActiveTriples::ProxyList
 
   class InvalidAggregatorObjectError < RuntimeError
   end
+
+  class InvalidProxyListGraph < RuntimeError
+  end
 end
 
 class ORE < RDF::Vocabulary('http://www.openarchives.org/ore/1.0/datamodel#'); end
-class FOBJ < RDF::Vocabulary('http://fedora.info/definitions/v4/models#'); end
 
-describe ActiveTriples::ProxyList do
-  subject { ActiveTriples::ProxyList.new(aggregator) }
+describe RDF::ProxyList do
+  subject { described_class.new(aggregator) }
   let(:aggregator) { RDF::URI('http://example.org/agg') }
   let(:uri) { RDF::URI('http://example.org') }
 
-  ##
-  # Queries a given predicate with a proxyable value returning the node the
-  # proxy stands in for.
-  def query_proxy_for(proxy_list, predicate)
-    query = RDF::Query.new do
-      pattern [proxy_list.aggregator, predicate, :proxy]
-      pattern [:proxy, ORE.proxyFor, :value]
-    end
-
-    query.execute(proxy_list.graph).map(&:value)
-  end
-
-  def query_next_node(proxy_list, current)
-    query = RDF::Query.new do
-      pattern [:current_proxy, ORE.proxyFor, current]
-      pattern [:current_proxy, ORE.next, :next_proxy]
-      pattern [:next_proxy, ORE.proxyFor, :next_node]
-    end
-
-    query.execute(proxy_list.graph).map(&:next_node)
-  end
-
   RSpec::Matchers.define :have_ore_first_of do |expected|
-    match { |actual| query_proxy_for(actual, ORE['first']) == [expected] }
+    match do |actual|
+      first = described_class.first_from_graph(actual.aggregator, actual.graph)
+      first == expected
+    end
   end
 
   RSpec::Matchers.define :have_ore_last_of do |expected|
-    match { |actual| query_proxy_for(actual, ORE.last) == [expected] }
+    match do |actual|
+      last = described_class.last_from_graph(actual.aggregator, actual.graph)
+      last == expected
+    end
   end
 
   RSpec::Matchers.define :have_ore_order_of do |*expected|
     match do |actual|
-      current = query_proxy_for(actual, ORE['first']).first
+      current = described_class.first_from_graph(actual.aggregator,
+                                                 actual.graph)
       expected.each do |item|
         return false unless current == item
-        current = query_next_node(actual, current)
-        current = current.first if current.count == 1
+        current = described_class.query_next_node(actual.graph, current)
       end
       true
     end
@@ -138,8 +204,72 @@ describe ActiveTriples::ProxyList do
     end
   end
 
+  shared_context 'with uri list' do
+    let(:uris) do
+      uris = []
+      10.times { |i| uris << RDF::URI('http://example.org') / i }
+      uris
+    end
+  end
+
   it 'will have an aggregator that is an RDF::Resource' do
     expect(subject.aggregator.to_uri).to eq('http://example.org/agg')
+  end
+
+  describe '.new' do
+    subject { described_class.new(aggregator, graph) }
+
+    context 'with empty graph' do
+      let(:graph) { RDF::Graph.new }
+
+      it 'initializes with empty list' do
+        expect(subject).to be_empty
+      end
+    end
+
+    context 'with graph without first node' do
+      let(:graph) do
+        RDF::Graph.new << RDF::Statement(RDF::Node.new, RDF::DC.title, 'moomin')
+      end
+
+      it 'initializes with empty list' do
+        expect(subject).to be_empty
+      end
+    end
+
+    context 'with graph with list items' do
+      include_context 'with uri list'
+
+      let(:graph) { described_class.new(aggregator).concat(uris).graph }
+
+      it do
+        expect(subject).to have_ore_order_of(*uris)
+      end
+    end
+  end
+
+  describe '.first_from_graph' do
+    it 'returns first item'
+
+    it 'raises error when more than one first is present'
+
+    it 'returns nil for empty list'
+  end
+
+  describe '.last_from_graph' do
+    it 'returns first item'
+
+    it 'raises error when more than one last is present'
+
+    it 'returns ?? for empty list'
+  end
+
+  describe '.query_next_node' do
+    it 'returns first item'
+
+    it 'raises error when more than one next is present'
+
+    it 'raises nil when there is no next item'
   end
 
   describe '#<<' do
@@ -162,6 +292,10 @@ describe ActiveTriples::ProxyList do
     it 'pushes items to list' do
       expect { subject.concat([uri, uri, uri]) }
         .to change { subject.count }.by(3)
+    end
+
+    it 'returns self' do
+      expect(subject.concat([uri, uri, uri])).to eq subject
     end
   end
 
@@ -188,13 +322,9 @@ describe ActiveTriples::ProxyList do
     end
 
     context 'with multiple elements' do
-      before { subject.concat(uris) }
+      include_context 'with uri list'
 
-      let(:uris) do
-        uris = []
-        10.times { |i| uris << RDF::URI('http://example.org') / i }
-        uris
-      end
+      before { subject.concat(uris) }
 
       it 'specifies last element' do
         expect(subject).to have_ore_first_of uris.first
