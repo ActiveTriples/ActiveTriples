7f9b74c104eacef0f934ba3efa34263e45725718
Trey Terrell <trey.terrell@oregonstate.edu>
2015-06-04 13:22:34 -0700

    Application Strategy spec and documentation.

--- lib/active_triples/application_strategy.rb
+++ lib/active_triples/application_strategy.rb
@@ -5,6 +5,9 @@ module ActiveTriples
   # interface.
   class ApplicationStrategy
     class << self
+      # @param [ActiveTriples::Resource, #property] resource A resource to copy
+      #   the property to.
+      # @param [ActiveTriples::Property] property The property to copy.
       def apply(resource, property)
         resource.property property.name, property.to_h
       end
---
+++ spec/active_triples/application_strategy_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+RSpec.describe ActiveTriples::ApplicationStrategy do
+  subject { described_class }
+
+  describe ".apply" do
+    it "should copy the property to the asset" do
+      asset = build_asset
+      property = build_property(name: "name", options: {:predicate => RDF::DC.title})
+
+      subject.apply(asset, property)
+
+      expect(asset).to have_received(:property).with(property.name, property.to_h)
+    end
+
+    def build_asset
+      object_double(ActiveTriples::Resource, :property => nil)
+    end
+
+    def build_property(name:, options:)
+      property = object_double(ActiveTriples::Property.new(:name => nil))
+      allow(property).to receive(:name).and_return(name)
+      allow(property).to receive(:to_h).and_return(options)
+      property
+    end
+  end
+end
