4425316d0078f10bd51afe92f196d54bae73e38c
Trey Terrell <trey.terrell@oregonstate.edu>
2015-06-04 13:34:35 -0700

    No required named attributes.

--- spec/active_triples/application_strategy_spec.rb
+++ spec/active_triples/application_strategy_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ActiveTriples::ApplicationStrategy do
   describe ".apply" do
     it "should copy the property to the asset" do
       asset = build_asset
-      property = build_property(name: "name", options: {:predicate => RDF::DC.title})
+      property = build_property("name", {:predicate => RDF::DC.title})
 
       subject.apply(asset, property)
 
@@ -17,7 +17,7 @@ RSpec.describe ActiveTriples::ApplicationStrategy do
       object_double(ActiveTriples::Resource, :property => nil)
     end
 
-    def build_property(name:, options:)
+    def build_property(name, options)
       property = object_double(ActiveTriples::Property.new(:name => nil))
       allow(property).to receive(:name).and_return(name)
       allow(property).to receive(:to_h).and_return(options)
