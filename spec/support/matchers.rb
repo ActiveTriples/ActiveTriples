d0e712e85a402296e1894946e960fff2b1d24611
Tom Johnson <tom@dp.la>
2015-07-10 14:37:46 -0700

    Add some tests for `#get_values`

---
+++ spec/support/matchers.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+RSpec::Matchers.define :be_a_relation_containing do |*expected|
+  match do |actual|
+    expect(actual.class).to eq ActiveTriples::Relation
+    expect(actual).to contain_exactly(*expected)
+    true
+  end
+end
+
