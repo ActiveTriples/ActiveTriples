37fd49534733e54a416b65beb7fc4c0f8b463760
Tom Johnson <tom@dp.la>
2016-03-24 11:42:41 -0700

    Allow PersistenceStrategy set by property config

--- spec/active_triples/relation_spec.rb
+++ spec/active_triples/relation_spec.rb
@@ -373,8 +373,24 @@ describe ActiveTriples::Relation do
             expect(subject.result.map(&:rdf_subject))
               .to contain_exactly(*values)
           end
+
+          context 'and persistence_strategy is configured' do
+            before do
+              reflections
+                .property :moomin, 
+                          predicate: RDF::URI('http://example.org/moomin'), 
+                          persist_to: ActiveTriples::RepositoryStrategy
+            end
+            
+            it 'assigns persistence strategy' do
+              subject.result.each do |node|
+                expect(node.persistence_strategy)
+                  .to be_a ActiveTriples::RepositoryStrategy
+              end
+            end
+          end
           
-          context 'when #cast? is false' do
+          context 'and #cast? is false' do
             let(:values) do
               [uri, RDF::URI('http://ex.org/too-ticky'), RDF::Node.new,
               'moomin', Date.today]
@@ -488,6 +504,20 @@ describe ActiveTriples::Relation do
         values = [:moomin, :snork]
         expect { subject.set(values) }
           .to change { subject.to_a }.to contain_exactly(*values)
+      end
+
+      context 'and persistence config' do
+        before do
+          reflections
+            .property :moomin, 
+                      predicate: RDF::URI('http://example.org/moomin'), 
+                      persist_to: ActiveTriples::RepositoryStrategy
+        end
+
+        it 'returns values with persistence strategy set' do
+          expect(subject.set(RDF::Node.new).map(&:persistence_strategy))
+            .to contain_exactly(an_instance_of(ActiveTriples::RepositoryStrategy))
+        end
       end
     end
   end
