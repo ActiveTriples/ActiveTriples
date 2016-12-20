e16233931cad5989c18b34e2cb90b5fc31bb2a65
Thomas Johnson <tom@dp.la>
2016-12-19 16:05:33 -0800

    Document `Schema` and `Property` (#253)
    Add YARD annotations for `ActiveTriples::Schema` and `ActiveTriples::Property`.

--- lib/active_triples/property.rb
+++ lib/active_triples/property.rb
@@ -4,40 +4,60 @@ module ActiveTriples
   # A value object to encapsulate what a Property is. Instantiate with a hash of
   # options.
   #
-  # @todo Should we enforce the interface on the various attributes that are set?
+  # @example configuring a property in a schema
+  #   title_prop = ActiveTriples::Property.new(name:     :title,
+  #                                            predicate: RDF::Vocab::DC.title)
   class Property
+    ##
+    # @param options [Hash] Options for the property
+    # @option options [RDF::URI] :name
+    # @option options [Boolean] :cast
+    # @option options [String, Class] :class_name
+    # @option options [RDF::URI] :predicate
     def initialize(options = {})
       self.name = options.fetch(:name)
       self.attributes = options.except(:name)
     end
 
-    # @return Symbol
+    ##
+    # @!attribute [r] name
+    #   @return [Symbol]
     attr_reader :name
 
-    # @return Boolean
+    ##
+    # @return [Boolean]
     def cast
       attributes.fetch(:cast, false)
     end
 
-    # @return Class
+    ##
+    # @return [Class]
     def class_name
       attributes[:class_name]
     end
 
-    # @return RDF::Vocabulary::Term
+    ##
+    # @return [RDF::Vocabulary::Term]
     def predicate
       attributes[:predicate]
     end
 
     private
 
-    attr_writer :name
+    ##
+    # @!attribute [w] name
+    #   @return [Symbol]
+    # @!attribute [rw] attributes
+    #   @return [Hash<Symbol, Object>]
+    attr_writer   :name
     attr_accessor :attributes
 
     alias_method :to_h, :attributes
 
-    # Returns the property's configuration values. Will not return #name, which is
-    # meant to only be accessible via the accessor.
+    ##
+    # Returns the property's configuration values. Will not return #name, which 
+    # is meant to only be accessible via the accessor.
+    # 
     # @return [Hash] Configuration values for this property.
     public :to_h
   end
